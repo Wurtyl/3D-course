669cd32fd9bd935c90ec91146b63388c90374ebe
Josh <159182037+jovazquez1@users.noreply.github.com>
2025-01-27 15:00:53 -0800

    L6 P1

--- learn-3d-gamedev-from-zero-v0.8.0-macos/M03.gobot_the_sorcerer_workbook/practices/L6.P1/bullet.gd
+++ learn-3d-gamedev-from-zero-v0.8.0-macos/M03.gobot_the_sorcerer_workbook/practices/L6.P1/bullet.gd
@@ -1,30 +1,22 @@
 extends Area3D
-
-# Constants are a way to define types locally. Doing this allows you
-# to use BulletSkin as a type hint without registering
-# the type throughout the project.
 const BulletSkin = preload("bullet_skin.gd")
-
-@export var bullet_skin_scene: PackedScene = null
-
+@export var bullet_skin_scene: PackedScene = preload("bullet_skin.tscn") 
 var speed := 10.0
 var max_range := 10.0
 var traveled_distance := 0.0
-
 var visual: BulletSkin = null
 
-
 func _ready() -> void:
-	# Store an instance of the skin in the visual variable,
-	# add it to the scene and play its appear animation.
-	pass
-
+	visual = bullet_skin_scene.instantiate() 
+	add_child(visual) 
+	visual.appear() 
 
 func _physics_process(delta: float) -> void:
-	# Move the bullet forward and update the traveled distance.
-	pass
-
-	
-	# Destroy the bullet when it travels past the maximum range.
+	var distance := speed * delta 
+	var motion := -transform.basis.z * distance 
+	position += motion 
+	traveled_distance += distance 
 	if traveled_distance > max_range:
-		pass
+		set_physics_process(false) 
+		visual.destroy() 
+		visual.tree_exited.connect(queue_free) 
